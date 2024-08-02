74422eeb5c2a16b334e3793e67d5cb37c4108515
Anatol Kaczmarek <anatol.kaczmarek@gmail.com>
2024-08-02 17:47:58 +0200

    switch api provider and add search bar suggestions

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     const [stocks, setStocks] = useState([]);
     const [symbol, setSymbol] = useState("");
     const [suggestions, setSuggestions] = useState([]);
+    const [enteredText, setEnteredText] = useState(''); 
 
     useEffect(() => {
         fetchStocks();
@@ -13,10 +14,18 @@ function App() {
 
     const fetchStocks = async () => {
         try {
-            const response = await fetch("http://127.0.0.1:8000/api/stocks/");
+            const response = await fetch("http://127.0.0.1:8000/api/stocks/").then((response) => {
+                if (response.status === 403) {
+                    throw new Error("Token limit reached");
+                }
+                if (response.status === 204) {
+                    throw new Error("No followed stocks");
+                }
+            });
             const data = await response.json();
             setStocks(data);
         } catch (error) {
+            alert(error);
             console.log(error);
         }
     }
@@ -28,10 +37,17 @@ function App() {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({ symbol }),
+            }).then((response) => { 
+                if (response.status === 403) {
+                    throw new Error("Token limit reached");
+                }
             });
             const data = await response.json();
             setStocks((prev) => [...prev, data]);
+            setEnteredText("");
+            setSuggestions([]);
         } catch (error) {
+            alert(error);
             console.log(error);
         }
     };
@@ -40,9 +56,14 @@ function App() {
             // eslint-disable-next-line no-unused-vars
             const response = await fetch(`http://127.0.0.1:8000/api/stocks/remove/${id}/`, {
                 method: "DELETE",
+            }).then((response) => {
+                if (response.status === 403) {
+                    throw new Error("Token limit reached");
+                }
             });
             setStocks((prev) => prev.filter((stock) => stock.id !== id));
         } catch (error) {
+            alert(error);
             console.log(error);
         }
     }
@@ -51,35 +72,49 @@ function App() {
     `;
     const fetchSuggestions = async (symbol) => {
         try {
-            const response = await fetch(`http://127.0.0.1:8000/api/stocks/suggestions/${symbol}/`);
+            const response = await fetch(`http://127.0.0.1:8000/api/stocks/suggestions/${symbol}/`).then((response) => {
+                if (response.status === 403) {
+                    throw new Error("Token limit reached");
+                }
+            });
             setSuggestions(await response.json());
         } catch (error) {
+            alert(error);
             console.log(error);
         }
     }
-    const onChange = (event) => {
+    const searchBarChange = (event) => {
+        setEnteredText(event.target.value);
         setSymbol(event.target.value);
         fetchSuggestions(event.target.value);
+        if (!event.target.value) {
+            setSuggestions([]);
+        }
+    }
+    const suggestionsClick = (symbol) => {
+        setSymbol(symbol);
+        setEnteredText(symbol);
     }
     return (
         <>
             <h1>Stock App</h1>
             <div className='search-container'>
                 <div className='search-inner'>
-                    <input type="text" placeholder="Stock Symbol..." onChange={onChange} />
+                    <input type="text" placeholder="Stock Symbol..." value={enteredText} onChange={searchBarChange} />
                     <button onClick={addStock}>Follow</button>
                 </div>
                 <div className='dropdown'>
                     {suggestions.length ? suggestions.map((suggestion, index) => (
-                        <div key={index} className='dropdown-row'>{suggestion.symbol}</div>
+                        <div key={index} className='dropdown-row' onClick={() => suggestionsClick(suggestion.symbol)}>{suggestion.symbol} ({suggestion.name})</div>
                     )) : null}
                 </div>
             </div>
             {stocks.map((stock, index) => (
                 <div key={index}>
                     <p>Symbol: {stock.symbol}</p>
+                    <p>Name: {stock.name}</p>
                     <p>Price: {stock.price}</p>
-                    <Change data={stock.change_percent}>Change: {stock.change_percent}</Change>
+                    <Change data={stock.changesPercentage}>Change: {stock.changesPercentage}</Change>
                     <button onClick={() => removeStock(stock.id)}>Unfollow</button>
                 </div>
             ))}
--- server/reactivestocks/api/views.py
+++ server/reactivestocks/api/views.py
@@ -1,66 +1,63 @@
-import re
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Stock
 from .serializer import StockSerializer
 import requests
-import pandas as pd
 from pandas.tseries.holiday import USFederalHolidayCalendar
 from pandas.tseries.offsets import CustomBusinessDay
 
 US_BUSINESS_DAY = CustomBusinessDay(calendar=USFederalHolidayCalendar())
-APIKEY = 'JOFNAS2BGJEJJOCD'
-
-
-def adjust_json_format(data):
-    data['symbol'] = data['Global Quote']['01. symbol']
-    data['open_price'] = data['Global Quote']['02. open']
-    data['high'] = data['Global Quote']['03. high']
-    data['low'] = data['Global Quote']['04. low']
-    data['price'] = data['Global Quote']['05. price']
-    data['volume'] = data['Global Quote']['06. volume']
-    data['latest_trading_day'] = data['Global Quote']['07. latest trading day']
-    data['previous_close'] = data['Global Quote']['08. previous close']
-    data['change'] = data['Global Quote']['09. change']
-    data['change_percent'] = data['Global Quote']['10. change percent'][:-1]
-    return data
+APIKEY = 'smwtbHsasmvEoGzGfDTq5Wo5xcqVHQvu'
 
 
 def update(stock):
     symbol = stock.symbol
     data = requests.get(
-        f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={APIKEY}').json()
-    data = adjust_json_format(data)
-    stock.open_price = data['open_price']
-    stock.high = data['high']
-    stock.low = data['low']
+        f'https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={APIKEY}').json()
+    if data['Error Message']:
+        return None
+    data = data[0]
     stock.price = data['price']
-    stock.volume = data['volume']
-    stock.latest_trading_day = data['latest_trading_day']
-    stock.previous_close = data['previous_close']
+    stock.changesPercentage = data['changesPercentage']
     stock.change = data['change']
-    stock.change_percent = data['change_percent']
+    stock.dayLow = data['dayLow']
+    stock.dayHigh = data['dayHigh']
+    stock.yearHigh = data['yearHigh']
+    stock.yearLow = data['yearLow']
+    stock.marketCap = data['marketCap']
+    stock.priceAvg50 = data['priceAvg50']
+    stock.priceAvg200 = data['priceAvg200']
+    stock.volume = data['volume']
+    stock.avgVolume = data['avgVolume']
+    stock.open = data['open']
+    stock.previousClose = data['previousClose']
+    stock.eps = data['eps']
+    stock.pe = data['pe']
+    stock.earningsAnnouncement = data['earningsAnnouncement']
+    stock.sharesOutstanding = data['sharesOutstanding']
+    stock.timestamp = data['timestamp']
     stock.save()
 
 
 @api_view(['GET'])
 def get_stocks(request):
+    if not Stock.objects.exists():
+        return Response(status=status.HTTP_204_NO_CONTENT)
     stocks = Stock.objects.all()
     for stock in stocks:
-        if stock.latest_trading_day != (pd.Timestamp.now().normalize() - US_BUSINESS_DAY).to_pydatetime().date():
-            update(stock)
+        if update(stock) is None:
+            return Response(status=status.HTTP_403_FORBIDDEN)
     serializedData = StockSerializer(Stock.objects.all(), many=True).data
     return Response(serializedData)
 
 
 @api_view(['GET'])
 def get_suggestions(request, symbol):
-    print(symbol)
     data = requests.get(
-        f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=${APIKEY}').json()
-    print(data)
+        f'https://financialmodelingprep.com/api/v3/search?query={symbol}&limit=5&apikey={APIKEY}').json()
+    if data['Error Message']:
+        return Response(data['Error Message'], status=status.HTTP_403_FORBIDDEN)
     return Response(data)
 
 
@@ -68,13 +65,13 @@ def get_suggestions(request, symbol):
 def add_stock(request):
     symbol = request.data['symbol']
     data = requests.get(
-        f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={APIKEY}').json()
-    data = adjust_json_format(data)
-    serializer = StockSerializer(data=data)
+        f'https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={APIKEY}').json()
+    if data['Error Message']:
+        return Response(data['Error Message'], status=status.HTTP_403_FORBIDDEN)
+    serializer = StockSerializer(data=data[0])
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
