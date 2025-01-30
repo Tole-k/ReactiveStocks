24330aad0c95204c7b24e7d099b8911fe7d936cc
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2025-01-30 02:59:10 +0100

    changes?

--- client/reactive-stocks/src/api.js
+++ client/reactive-stocks/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ACCESS_TOKEN } from './constants';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from './constants';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -14,6 +14,42 @@ api.interceptors.request.use((config) => {
 },
     (error) => {
         return Promise.reject(error)
+    }
+);
+api.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    async function (error) {
+
+        const originalRequest = error.config;
+
+        if (error.response && (error.response.status === 403 || error.response.status == 401 )&& !originalRequest._retry) {
+            originalRequest._retry = true;
+            const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+            if (!refreshToken) {
+                return Promise.reject(error);
+            }
+            try {
+                const response = await api.post('http://localhost:8000/token/refresh/', {
+                    refresh: refreshToken
+                });
+
+                if (response.status === 200) {
+                    localStorage.setItem(ACCESS_TOKEN, response.data.access);
+                    localStorage.setItem(REFRESH_TOKEN, response.data.refresh);
+                    console.log('Token refreshed');
+
+                    originalRequest.headers['Authorization'] = `Bearer ${response.data.accessToken}`;
+
+                    return api(originalRequest);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        return Promise.reject(error);
     }
 );
 export default api
